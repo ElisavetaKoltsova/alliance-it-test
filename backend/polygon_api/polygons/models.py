1ed4521bb8501d449c60e9b37802034cca0997db
kuricakaka <e.koltsova@yadro.com>
2026-09-17 21:29:44 +0300

    [TEST] Add polygon model

--- backend/polygon_api/polygons/models.py
+++ backend/polygon_api/polygons/models.py
@@ -1,3 +1,10 @@
-from django.db import models
+from django.contrib.gis.db import models
 
-# Create your models here.
+
+class Polygon(models.Model):
+    name = models.CharField(max_length=255)
+    geometry = models.PolygonField(srid=4326)
+    crosses_antimeridian = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
